6af02601b913213a64763d0f689de56ca6c39487
Elizabeth Yalkut <elizabeth_yalkut@brown.edu>
2025-03-05 16:08:37 -0500

    images from the BDR are on the map and skewed 'properly' (using three points)

--- assets/js/leaflet.js
+++ assets/js/leaflet.js
@@ -1,8 +1,9 @@
 let map = L.map('map', {
     minZoom: 5,
+    maxZoom: 20,
     //attributionControl: false,
     zoomControl: true,
-}).setView([29.9415, 31.1449], 11);
+}).setView([30.407, 30.368], 8);
 const basemaps = {
     Topo: L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
         attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
@@ -21,24 +22,14 @@ const basemaps = {
 };
 basemaps.Satellite.addTo(map);
 
-// api
-//let bdroverlay = L.imageOverlay("https://repository.library.brown.edu/studio/thumbnail/bdr:6cnew3p2", gizaBounds, { opacity: 0.75, interactive: true });
-// local manipulated img
-//let bdroverlay = L.imageOverlay("/804/warped.png",[gizaBounds], {opacity:0.75, interactive: true});
-//let rect = L.polygon(gizaBounds, { fillOpacity: 0, color: "red" });
-/*rect.on('click', function(b) {
-    event.preventDefault(); //to avoid changes the current page url
-    window.open('https://repository.library.brown.edu/viewers/mirador/bdr:6cnew3p2/'); //the behavior is defined by the browser and user options
-    return false; // prevents the default action associated with the event
-
-});
-let laurel = L.featureGroup([rect, bdroverlay]);
-*/
 let box = {
     "color": "red",
     "weight": 2,
     "fillOpacity": 0,
 };
+let highlight = {
+    "color": "yellow",
+}
 
 let flightLayer = L.featureGroup();
 let flightPaths = new L.GeoJSON.AJAX("/B8649_flightpath.geojson", {
@@ -70,7 +61,7 @@ let bdrjson = new L.GeoJSON.AJAX("/bdr.geojson", {
         if (pid.startsWith('bdr')) {
             let bdrViewer = "https://repository.library.brown.edu/studio/item/" + pid;
             let bdrThumb = "https://repository.library.brown.edu/studio/thumbnail/" + pid;
-
+            layer.bindPopup(pid);
             // draw the outline
             layer.setStyle(box);
             // add the image overlay; if the bounding box is not a rectangle, it will not fit
@@ -91,30 +82,40 @@ let bdrjson = new L.GeoJSON.AJAX("/bdr.geojson", {
             });
         }
     }
-});
-*/
+});*/
 
-let bounding_boxes = L.featureGroup();
-let bboxes = new L.GeoJSON.AJAX("/bbox_sample.geojson", {
-    // build each polygon
+
+let bdr = L.featureGroup();
+let boxes = new L.GeoJSON.AJAX("/bbox_sample.geojson", {
     onEachFeature:function(feature,layer) {
-        // draw the outline
-        layer.setStyle(box);
-        layer.bindPopup("Frame: " + feature.properties.Frame);
-
-        layer.addTo(bounding_boxes);
-
+        // get BDR pid for each set of coordinates so we can grab the image from there; we don't need hi-res images here 
+        let pid = feature.properties.pid;
+        let bdrThumb = "https://repository.library.brown.edu/studio/thumbnail/" + pid;
+        // link to BDR item
+        let bdrViewer = "https://repository.library.brown.edu/studio/item/" + pid;
+        
+        let geoArray = feature.geometry.coordinates;
+        // we have to take the arrays of coordinates from the geojson and flip them to be lon/lat for the rotated image overlay.
+        // why? no one knows. why is it 1-3-2? again: no one knows. the imageOverlayRotated plugin calls the required coordinates
+        // topLeft, topRight, bottomLeft, but this may or may not correspond to the actual points on the map, so I've used more-generic words
+        let first = geoArray[0][0][0].reverse();
+        let second = geoArray[0][0][1].reverse();
+        let third = geoArray[0][0][3].reverse();
+        
+        let image = L.imageOverlay.rotated(bdrThumb, first, second, third, {opacity:0.5, interactive: true});
+        image.addTo(bdr);
+        
+        layer.addTo(bdr).setStyle(box);
     }
-
 });
+
 
 // establish the overlays
 let overlayMaps = {
     "Flights": flightLayer,
     //"Demo": combo,
-    "Bounding boxes": bounding_boxes
+    "MVP": bdr
 };
-
 // Allow user to choose what overlays to display
 const layerControl = L.control.layers(basemaps, overlayMaps, { collapsed: false, position: 'topright' }).addTo(map);
 
